8b1acc914321cd4b1a8a5c63518461344ce0cef9
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-08-30 11:14:55 +0300

    learn: learning iteration in \go with tests

--- Integers/adder.go
+++ Integers/adder.go
@@ -1,10 +1,19 @@
 package integers
 
+import "fmt"
+
 func Add(x, y int) int {
-	return 0
+	//Add function adds two integers and returns there total/sum
+	return x + y
 }
 
 func integers() {
 	Add(2, 2)
 
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
