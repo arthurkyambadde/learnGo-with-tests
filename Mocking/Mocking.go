52af6fadfc83b56979fce6c05f19fb9ed403e435
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-09-06 21:15:31 +0300

    learn: learning mocking sleep function

--- Mocking/Mocking.go
+++ Mocking/Mocking.go
@@ -6,10 +6,30 @@ import (
 	"time"
 )
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type Defaultsleeper struct {
+}
+
+func (d *Defaultsleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := 3; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+
+		sleeper.Sleep()
+
 	}
 	fmt.Fprint(out, "Go!")
 
